e55522f47b2ecd675b594ac52ea8b24c11ec886c
4OO5k <richardson0705@yahoo.com>
2025-03-06 18:33:57 -0500

    .

--- DR - Shape Jam/Assets/Scripts/EnemyEmitterController.cs
+++ DR - Shape Jam/Assets/Scripts/EnemyEmitterController.cs
@@ -43,6 +43,10 @@ public class EnemyEmitterController : MonoBehaviour
                 Instantiate(Enemy, enemyPosition, transform.rotation);
             }
 
+            if ()
+            {
+
+            }
 
             /*****************************\
             |**** Add your code above ****|
